36e43800cfe2c5d0b3c087e8c5c8fb486f3f359d
Invincible <87626336+vbmisra@users.noreply.github.com>
2021-11-20 00:38:19 -0800

    committing changes to index.js

--- index.js
+++ index.js
@@ -7,40 +7,110 @@ const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 
+//inquirer questions
+const manangerQuestions = [
+    {
+        type: 'input',
+        message: "Please enter Team Manager's name: ",
+        name: 'mgrName',
+    },
+    {
+        type: 'input',
+        message: "Enter manager's employee ID: ",
+        name: 'mgrID',
+    },
+    {
+        type: 'input',
+        message: "Enter manager's email address: ",
+        name: 'mgrEmail',
+    },
+    {
+        type: 'input',
+        message: "Enter manager's office phone number: ",
+        name: 'mgrPhone',
+    },
+    {
+        type: 'list',
+        message: "What would you like to do next?",
+        choices: ['add Engineer', 'add Intern', 'finish building team'],
+        name: 'nextChoice'
+    },
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "Please enter Engineer's name: ",
+        name: 'engName',
+    },
+    {
+        type: 'input',
+        message: "Enter Engineer's employee ID: ",
+        name: 'engID',
+    },
+    {
+        type: 'input',
+        message: "Enter manager's email address: ",
+        name: 'engEmail',
+    },
+    {
+        type: 'input',
+        message: "Enter Engineer's Github username: ",
+        name: 'engGithub',
+    },
+    {
+        type: 'list',
+        message: "What would you like to do next?",
+        choices: ['add Engineer', 'add Intern', 'finish building team'],
+        name: 'nextChoice'
+    },
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "Please enter Intern's name: ",
+        name: 'intName',
+    },
+    {
+        type: 'input',
+        message: "Enter Intern's employee ID: ",
+        name: 'intID',
+    },
+    {
+        type: 'input',
+        message: "Enter Intern's email address: ",
+        name: 'intEmail',
+    },
+    {
+        type: 'input',
+        message: "Enter the school Intern attends: ",
+        name: 'intSchool',
+    },
+    {
+        type: 'list',
+        message: "What would you like to do next?",
+        choices: ['add Engineer', 'add Intern', 'finish building team'],
+        name: 'nextChoice'
+    },
+]
+
+// const nextQuestion = [
+//     {
+//         type: 'list',
+//         message: "What would you like to do next?",
+//         choices: ['add Engineer', 'add Intern', 'finish building team'],
+//         name: 'nextChoice'
+//     },
+// ]
+
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: "Please enter Team Manager's name: ",
-            name: 'managerName',
-        },
-        {
-            type: 'input',
-            message: "Enter manager's employee ID: ",
-            name: 'managerID',
-        },
-        {
-            type: 'input',
-            message: "Enter manager's email address: ",
-            name: 'managerEmail',
-        },
-        {
-            type: 'input',
-            message: "Enter manager's office phone number: ",
-            name: 'managerPhone',
-        },
-        {
-            type: 'list',
-            message: "What would you like to do next?",
-            choices: ['add Engineer', 'add Intern', 'finish building team'],
-            name: 'nextChoice'
-        },
-    ])
+    .prompt(manangerQuestions)
     .then ((answers) => {
         if(answers.nextChoice == 'add Engineer') {
-            console.log(`${answers.managerName} chose to add an Engineer`);
+            console.log(`${answers.mgrName} chose to add an Engineer`);
         } else if(answers.nextChoice == 'add Intern') {
-            console.log(`${answers.managerName} chose to add an Intern!`);
+            console.log(`${answers.mgrName} chose to add an Intern!`);
         } else {
             // (answers) => {
             //     const htmlPageContent = generateHTML(answers);
